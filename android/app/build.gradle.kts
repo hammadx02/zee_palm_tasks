9d7c43ecaec33437f1d207c164216999616a89d9
Hammad Ali <hammadaali02@gmail.com>
2025-06-04 18:44:46 +0500

    refactor: Remove unused platform entries from metadata and specify NDK version in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.zee_palm_tasks"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
